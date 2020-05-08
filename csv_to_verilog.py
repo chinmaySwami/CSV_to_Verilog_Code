1db0f5329b704b7aa2e673af471983805204ec9f
Chinmay Swami <chinmayasami@gmail.com>
2020-05-08 17:52:25 -0400

    wrote the code to convert pandas df to string

--- csv_to_verilog.py
+++ csv_to_verilog.py
@@ -5,11 +5,13 @@ df = pd.read_csv('DT_MDL4.csv', index_col=False,
                                        "c_ID", "c_elif", "c_condition", "c_then", "c_node", "cc_ID",
                                        "cc_else","f_val"])
 
+# print(df.columns)
+# print(df)
 
-print(df.columns)
-print(df)
+df["ID"] = df["ID"].astype('float32')
+
 df = df.astype(str)
-df = df.replace({'p_then': {'then': 'begin'}, 'c_then':{'then': 'begin'}})
+df = df.replace({'p_then': {'then': 'begin'}, 'c_then':{'then': 'begin'}, 'c_elif':{'elseif': 'else if'}})
 
 df['p_if/class'] = df['p_if/class'] + " ("+df['p_condition']+") "+df['p_then']
 
@@ -17,14 +19,27 @@ df['c_elif'] = df['c_elif'] + " ("+df['c_condition']+") "+df['c_then']
 
 # df = df.replace({'p_if/class': r'^class.*$'}, {'p_if/class': 'class'}, regex=True)
 
-print(df)
-
 ndf = df[['ID', 'p_if/class', 'c_ID', 'c_elif', 'cc_ID', 'cc_else', 'f_val']]
 
-print(ndf)
-
 condition_dict = {}
-for index, row in ndf:
+for index, row in ndf.iterrows():
     condition_dict[row[0]] = [row[1], row[2]]
 
-print(condition_dict)
+code = ""
+for index, row in ndf.iterrows():
+    if index == 0:
+        code += row[1] + "\n "+ "$*"+str(row[2])+"*$"+" \n"+"end \n"
+        code += row[3] + "\n" + "$*" + str(row[4]) + "*$" + "\n" + "end \n"
+    else:
+        if not 'class' in row[1]:
+            strs1 =  row[1] + "\n"+ "$*"+str(row[2])+"*$"+"\n"+"end \n"
+            strs2 = row[3] + "\n" + "$*" + str(row[4]) + "*$" + "\n" + "end \n"
+
+            code = code.replace("$*"+str(row[0])+"*$", strs1+strs2)
+        else:
+            strs = row[1].replace('(=)', '=') + ';'
+            code = code.replace("$*" + str(row[0]) + "*$", strs)
+
+
+
+print(code)
