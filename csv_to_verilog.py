0c2ec66ce463faea75b95f8969946f3dc65bcd81
Chinmay Swami <chinmayasami@gmail.com>
2020-05-08 16:18:36 -0400

    reduced the number of columns in dictionary

--- csv_to_verilog.py
+++ csv_to_verilog.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 df = pd.read_csv('DT_MDL4.csv', index_col=False,
                  names=["ID", "p_if/class", "p_condition", "p_then", "p_node",
-                                       "c_ID", "c_elif", "c_condition", "c_then", "c_node", "cc_id",
+                                       "c_ID", "c_elif", "c_condition", "c_then", "c_node", "cc_ID",
                                        "cc_else","f_val"])
 
 
@@ -15,6 +15,16 @@ df['p_if/class'] = df['p_if/class'] + " ("+df['p_condition']+") "+df['p_then']
 
 df['c_elif'] = df['c_elif'] + " ("+df['c_condition']+") "+df['c_then']
 
-df = df.replace({'p_if/class': r'^class.*$'}, {'p_if/class': 'class'}, regex=True)
+# df = df.replace({'p_if/class': r'^class.*$'}, {'p_if/class': 'class'}, regex=True)
 
 print(df)
+
+ndf = df[['ID', 'p_if/class', 'c_ID', 'c_elif', 'cc_ID', 'cc_else', 'f_val']]
+
+print(ndf)
+
+condition_dict = {}
+for index, row in ndf:
+    condition_dict[row[0]] = [row[1], row[2]]
+
+print(condition_dict)
