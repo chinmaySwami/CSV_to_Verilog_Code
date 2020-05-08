22cb89dc152e067f8b3865bc181c8675045e145c
Chinmay Swami <chinmayasami@gmail.com>
2020-05-08 15:16:20 -0400

    cleaned and generated new columns using regex

--- csv_to_verilog.py
+++ csv_to_verilog.py
@@ -1,4 +1,20 @@
 import pandas as pd
 
-df = pd.read_csv('DT_MDL4.csv')
+df = pd.read_csv('DT_MDL4.csv', index_col=False,
+                 names=["ID", "p_if/class", "p_condition", "p_then", "p_node",
+                                       "c_ID", "c_elif", "c_condition", "c_then", "c_node", "cc_id",
+                                       "cc_else","f_val"])
+
+
+print(df.columns)
+print(df)
+df = df.astype(str)
+df = df.replace({'p_then': {'then': 'begin'}, 'c_then':{'then': 'begin'}})
+
+df['p_if/class'] = df['p_if/class'] + " ("+df['p_condition']+") "+df['p_then']
+
+df['c_elif'] = df['c_elif'] + " ("+df['c_condition']+") "+df['c_then']
+
+df = df.replace({'p_if/class': r'^class.*$'}, {'p_if/class': 'class'}, regex=True)
+
 print(df)
